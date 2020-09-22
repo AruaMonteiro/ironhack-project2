559d93b776997e29ccb40e5f2505d3c08f83a6d9
Camila Fassarella <fassarella.camila@gmail.com>
2020-09-22 09:37:58 -0300

    tuesday 1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/marmitas", (req, res, next) => {
+  const marmitas = [
+    {
+      name: "marmitas 1",
+      category: "comida brasileira",
+      image: "/images/marmita.jpg",
+    },
+    {
+      name: "marmitas 2",
+      category: "comida brasileira",
+      image: "/images/marmita.jpg",
+    },
+    {
+      name: "marmitas 3",
+      category: "comida brasileira",
+      image: "/images/marmita.jpg",
+    },
+    {
+      name: "marmitas 4",
+      category: "comida brasileira",
+      image: "/images/marmita.jpg",
+    },
+    {
+      name: "marmitas 5",
+      category: "comida brasileira",
+      image: "/images/marmita.jpg",
+    },
+    {
+      name: "marmitas 6",
+      category: "comida brasileira",
+      image: "/images/marmita.jpg",
+    },
+  ];
+  console.log(marmitas);
+  res.render("marmitas", { marmitas });
 });
 
 module.exports = router;
